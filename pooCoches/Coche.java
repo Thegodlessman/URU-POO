24891a6f9cfb1087cba612ebb9274677665c42d8
Luis Chang <90518199+Thegodlessman@users.noreply.github.com>
2022-02-12 15:42:48 -0400

    Coche.java Actualizado

--- pooCoches/Coche.java
+++ pooCoches/Coche.java
@@ -1,56 +1,58 @@
-package pooCoches;
+	package pooCoches;
 
 public class Coche {
 	
-	private int ruedas;
-	private int  largo;
-	private int  ancho;
-	private int  motor; 
-	private int  peso;
+	private String ruedas;
+	private String  largo;
+	private String  ancho;
+	private String  motor; 
+	private String  peso;
+	
 	
 	//METODO CONSTRUCTOR -- Le da condiciones iniciales 
 	
 	public Coche() {
 		
-		ruedas = 4; 
-		largo = 2000;
-		ancho = 300;
-		motor = 1600;
-		peso = 550;
+		ruedas = "4"; 
+		largo = "2000";
+		ancho = "300";
+		motor = "1600";
+		peso = "550";
+		
 		
 	}
 	
 	//Getter
 	public String getRuedas() {
-		return "El numero de ruedas es: " + ruedas;
+		return "La camioneta tiene: "+ruedas+" Ruedas";  
 	}
-	public String getLargo() {
-		return "El largo del coche es: " + largo;
+	public String getLargo(){
+		return "La camioneta tiene un largo de: "+largo+" Unidades";
 	}
-	public String getAncho() {
-		return "El largo del coche es: " + ancho;
+	public String getAncho(){
+		return "La camioneta tiene un ancho de: "+ancho+" Unidades";
 	}
-	public String getMotor() {
-		return "El largo del coche es: " + motor;
+	public String getMotor(){
+		return "La camioneta tiene un motor de: "+motor+" Unidades de potencia";
 	}
-	public String getPeso() {
-		return "El largo del coche es: " + peso;
+	public String getPeso(){
+		return "La comioneta tiene un peso de: "+peso+" Unidades";
 	}
 	
 	//Setter
-	public void setRuedas(int nuevo_ruedas) {
+	public void setRuedas(String nuevo_ruedas) {
 		this.ruedas = nuevo_ruedas;
 	}
-	public void setLargo(int nuevo_largo) {
+	public void setLargo(String nuevo_largo) {
 		this.largo = nuevo_largo;
 	}
-	public void setAncho(int nuevo_ancho) {
+	public void setAncho(String nuevo_ancho) {
 		this.ancho = nuevo_ancho;
 	}
-	public void setMotor(int nuevo_motor) {
+	public void setMotor(String nuevo_motor) {
 		this.motor = nuevo_motor;
 	}
-	public void setPeso(int nuevo_peso) {
+	public void setPeso(String nuevo_peso) {
 		this.peso = nuevo_peso;
 	}
 }
